eac24553e8fca85ee2e3e131ccc9d7791abcdecb
untamemadman <sam@untamemediagroup.com>
2019-03-20 07:44:51 +0000

    Started working

---
+++ Assets/BulletMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<SphereCollider>().enabled = true;
+        Destroy(gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.GetComponent<Rigidbody>().AddForce(transform.forward);
+    }
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerMove : MonoBehaviour
+{
+    public int player;
+    
+    private Vector3 movementVector;
+    
+    private CharacterController characterController;
+    
+    private float movementSpeed = 100;
+    
+    public GameObject BulletPrefab;
+    public Transform BulletSpawn;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector.x = Input.GetAxis("LeftJoystickX_P" + player) * movementSpeed;
+        movementVector.z = Input.GetAxis("LeftJoystickY_P" + player) * movementSpeed;
+        movementVector.y = 0;
+        
+        characterController.Move(movementVector * Time.deltaTime);
+
+        if (Input.GetButtonDown("A_P" + player))
+        {
+            FireBullet();
+        }
+    }
+    
+    void FireBullet()
+    	{
+    		GameObject bullet = (GameObject)Instantiate(BulletPrefab, BulletSpawn.position, BulletSpawn.rotation);
+            bullet.GetComponent<Renderer>().material = this.GetComponent<Renderer>().material;
+    		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 10;
+    	}
+}
